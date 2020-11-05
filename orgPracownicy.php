53dc9ed81b6f200f83f84ea2b0d4e905d23d4171
Joanna Dyl <73647813+DylJoanna022@users.noreply.github.com>
2020-11-05 14:52:30 +0100

    Update orgPracownicy.php

--- orgPracownicy.php
+++ orgPracownicy.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+     <div>
+    <a href="https://dyljoanna.herokuapp.com/">Strona glowna</a><br/>
+    <a href="funkcAgregujace.php">funkcAgregujace.php</a><br/>
+    </div>
+
 <?php
 echo("Jestem tu: <br/>orgPracownicy.php<br/>");
 ?>
+  </body>
+</html>
