0861d5f9645110709b655845c5486d8730aa2d78
Joanna Dyl <73647813+DylJoanna022@users.noreply.github.com>
2020-11-05 14:24:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -69,6 +69,26 @@ while($row = mysqli_fetch_assoc($result)) {
     echo ('</tr>');
 }
 echo ('</table>');
+    
+$sql = "SELECT imie,zarobki,data_urodzenia,nazwa_dzial FROM pracownicy,organizacja WHERE id_org=dzial";
+echo("<h3>ZADANIE 1</h3>");
+echo("<li>".$sql."<br><br>");
+
+$result = mysqli_query($conn, $sql);
+     if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1" class="tabelka_moja">');
+echo ("<tr><th>imie</th><th>zarobki</th><th>data urodzenia</th><th>nazwa_dzial</th></tr>");
+while($row = mysqli_fetch_assoc($result)) {
+    echo ('<tr>');
+    echo ("<td>".$row['imie']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td><td>".$row['nazwa_dzial']."</td>");
+    echo ('</tr>');
+}
+echo ('</table>');
 
 ?>
 
