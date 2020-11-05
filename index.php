8729c0d79eb63d51e05794314be1e375e69468e7
Joanna Dyl <73647813+DylJoanna022@users.noreply.github.com>
2020-11-05 14:40:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,27 +12,6 @@
 <?php
 echo("<h1>Joanna Dyl nr 4</h1>");
 
-require_once "connect.php";
-
-$sql = "SELECT * FROM pracownicy";
-echo("<h3>ZADANIE 1</h3>");
-echo("<li>".$sql."<br><br>");
-
-$result = mysqli_query($conn, $sql);
-     if ( $result) {
-        echo "<li>ok";
-    } else {
-      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-    }
-
-echo('<table border="1" class="tabelka_moja">');
-echo ("<tr><th>imie</th><th>zarobki</th><th>data urodzenia</th><th>dzial</th></tr>");
-while($row = mysqli_fetch_assoc($result)) {
-    echo ('<tr>');
-    echo ("<td>".$row['imie']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td><td>".$row['dzial']."</td>");
-    echo ('</tr>');
-}
-echo ('</table>');
     
 $sql = "SELECT * FROM pracownicy WHERE imie NOT LIKE '%a'";
 echo("<h3>ZADANIE 2</h3>");
