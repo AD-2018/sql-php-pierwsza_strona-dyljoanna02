aef005f74fc99044360dabd31aebc4e2e8cbc9da
Joanna Dyl <73647813+DylJoanna022@users.noreply.github.com>
2020-11-05 14:37:49 +0100

    Update funkcAgregujace.php

--- funkcAgregujace.php
+++ funkcAgregujace.php
@@ -1,3 +1,23 @@
 <?php
-echo("Jestem tu: funkcAgregujace.php");
+echo("Jestem tu: <br>funkcAgregujace.php<br>");
+
+$sql = "SELECT * FROM pracownicy";
+echo("<h3>ZADANIE 1</h3>");
+echo("<li>".$sql."<br><br>");
+
+$result = mysqli_query($conn, $sql);
+     if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1" class="tabelka_moja">');
+echo ("<tr><th>imie</th><th>zarobki</th><th>data urodzenia</th><th>dzial</th></tr>");
+while($row = mysqli_fetch_assoc($result)) {
+    echo ('<tr>');
+    echo ("<td>".$row['imie']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td><td>".$row['dzial']."</td>");
+    echo ('</tr>');
+}
+echo ('</table>');
 ?>
