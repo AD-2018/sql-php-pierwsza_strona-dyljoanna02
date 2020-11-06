81252ab777b9ac969dff9fc18f7cb8f11e6a93bd
Joanna Dyl <73647813+DylJoanna022@users.noreply.github.com>
2020-11-06 11:47:31 +0100

    dodanie tabel

--- index.php
+++ index.php
@@ -11,6 +11,68 @@
     </div>
 <?php
 echo("<h1>Joanna Dyl nr 4</h1>");
+    
+    require_once "connect.php";
+
+$sql = "SELECT * FROM pracownicy WHERE dzial=2";
+echo("<h3>ZADANIE 1</h3>");
+echo("<li>".$sql."<br><br>");
+
+$result = mysqli_query($conn, $sql);
+     if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1" class="tabelka_moja">');
+echo ("<tr><th>imie</th><th>zarobki</th><th>data urodzenia</th><th>dzial</th></tr>");
+while($row = mysqli_fetch_assoc($result)) {
+    echo ('<tr>');
+    echo ("<td>".$row['imie']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td><td>".$row['dzial']."</td>");
+    echo ('</tr>');
+}
+echo ('</table>');
+
+$sql = "SELECT * FROM pracownicy WHERE dzial=2 or dzial=3";
+echo("<h3>ZADANIE 2</h3>");
+echo("<li>".$sql."<br><br>");
+
+$result = mysqli_query($conn, $sql);
+     if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1" class="tabelka_moja">');
+echo ("<tr><th>imie</th><th>zarobki</th><th>data urodzenia</th><th>dzial</th></tr>");
+while($row = mysqli_fetch_assoc($result)) {
+    echo ('<tr>');
+    echo ("<td>".$row['imie']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td><td>".$row['dzial']."</td>");
+    echo ('</tr>');
+}
+echo ('</table>');
+
+$sql = "SELECT * FROM pracownicy WHERE zarobki<30";
+echo("<h3>ZADANIE 3</h3>");
+echo("<li>".$sql."<br><br>");
+
+$result = mysqli_query($conn, $sql);
+     if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1" class="tabelka_moja">');
+echo ("<tr><th>imie</th><th>zarobki</th><th>data urodzenia</th><th>dzial</th></tr>");
+while($row = mysqli_fetch_assoc($result)) {
+    echo ('<tr>');
+    echo ("<td>".$row['imie']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td><td>".$row['dzial']."</td>");
+    echo ('</tr>');
+}
+echo ('</table>');
 ?>
 
 </body>
