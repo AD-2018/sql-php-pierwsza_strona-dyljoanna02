31fb6e473185da64b4780cd03433d1d16055b922
Joanna Dyl <73647813+DylJoanna022@users.noreply.github.com>
2020-11-06 11:53:19 +0100

    Update orgPracownicy.php

--- orgPracownicy.php
+++ orgPracownicy.php
@@ -32,6 +32,26 @@ while($row = mysqli_fetch_assoc($result)) {
     echo ('</tr>');
 }
 echo ('</table>');
+     
+$sql = "SELECT imie,nazwa_dzial FROM pracownicy,organizacja WHERE (id_org=dzial) AND (dzial=1 OR dzial=4) ";
+echo("<h3>ZADANIE 2</h3>");
+echo("<li>".$sql."<br><br>");
+
+$result = mysqli_query($conn, $sql);
+     if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1" class="tabelka_moja">');
+echo ("<tr><th>imie</th><th>nazwa_dzial</th></tr>");
+while($row = mysqli_fetch_assoc($result)) {
+    echo ('<tr>');
+    echo ("<td>".$row['imie']."</td><td>".$row['nazwa_dzial']."</td>");
+    echo ('</tr>');
+}
+echo ('</table>');
 ?>
   </body>
 </html>
