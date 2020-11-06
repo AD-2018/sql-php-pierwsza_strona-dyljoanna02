e67fd1e3c88e27aea85a73644a1ec75279efbe40
Joanna Dyl <73647813+DylJoanna022@users.noreply.github.com>
2020-11-06 12:19:23 +0100

    Update orgPracownicy.php

--- orgPracownicy.php
+++ orgPracownicy.php
@@ -175,7 +175,7 @@ while($row = mysqli_fetch_assoc($result)) {
 }
 echo ('</table>');
      
-$sql = "SELECT imie,zarobki,nazwa_dzial FROM pracownicy,organizacja WHERE (id_org=dzial) AND (imie NOT LIKE '%a') ORDER BY nazwa_dial asc AND zarobki asc";
+$sql = "SELECT imie,zarobki,nazwa_dzial FROM pracownicy,organizacja WHERE (id_org=dzial) AND (imie NOT LIKE '%a') ORDER BY nazwa_dial asc, zarobki asc";
 echo("<h3>ZADANIE 5</h3>");
 echo("<li>".$sql."<br><br>");
 
