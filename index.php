84db4b8f4fdcce25fb2892e810c6b5014efa945c
Joanna Dyl <73647813+DylJoanna022@users.noreply.github.com>
2020-11-06 11:42:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
 <body>
     <div>
     <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-dyljoanna02">Github</a><br/>
-    <a href="orgPracownicy.php">orgPracownicy.php</a><br/>
-    <a href="funkcAgregujace.php">funkcAgregujace.php</a><br/>
+    <a href="orgPracownicy.php">Pracownicy i Organizacja </a>
+    <a href="funkcAgregujace.php">Funkcje Agregujace</a><br/>
     </div>
 <?php
 echo("<h1>Joanna Dyl nr 4</h1>");
