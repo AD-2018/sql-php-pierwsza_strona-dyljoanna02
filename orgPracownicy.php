01f91b0957322909a88dbd4d6924c56bfa64c618
Joanna Dyl <73647813+DylJoanna022@users.noreply.github.com>
2020-11-06 12:02:01 +0100

    Update orgPracownicy.php

--- orgPracownicy.php
+++ orgPracownicy.php
@@ -74,7 +74,29 @@ while($row = mysqli_fetch_assoc($result)) {
 echo ('</table>');
      
 $sql = "SELECT imie,nazwa_dzial FROM pracownicy,organizacja WHERE id_org=dzial AND imie NOT LIKE '%a'";
-echo("<h3>ZADANIE 3</h3>");
+echo("<h3>ZADANIE 4</h3>");
+echo("<li>".$sql."<br><br>");
+
+$result = mysqli_query($conn, $sql);
+     if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1" class="tabelka_moja">');
+echo ("<tr><th>imie</th><th>nazwa_dzial</th></tr>");
+while($row = mysqli_fetch_assoc($result)) {
+    echo ('<tr>');
+    echo ("<td>".$row['imie']."</td><td>".$row['nazwa_dzial']."</td>");
+    echo ('</tr>');
+}
+echo ('</table>');
+     
+     echo("<h2>SORTOWANIE</h2>");
+     
+$sql = "SELECT imie,nazwa_dzial FROM pracownicy,organizacja WHERE id_org=dzial ORDER BY imie desc";
+echo("<h3>ZADANIE 1</h3>");
 echo("<li>".$sql."<br><br>");
 
 $result = mysqli_query($conn, $sql);
