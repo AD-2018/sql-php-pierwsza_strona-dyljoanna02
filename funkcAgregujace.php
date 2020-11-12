2f017daef94d4ab01d8caf021b6fcafa1f268a37
Joanna Dyl <73647813+DylJoanna022@users.noreply.github.com>
2020-11-12 11:05:08 +0100

    Update funkcAgregujace.php

--- funkcAgregujace.php
+++ funkcAgregujace.php
@@ -133,6 +133,26 @@ while($row = mysqli_fetch_assoc($result)) {
     echo ('</tr>');
 }
 echo ('</table>');
+     
+$sql = "SELECT count(imie) as liczba_pracownikow FROM pracownicy";
+echo("<h3>ZADANIE 7</h3>");
+echo("<li>".$sql."<br><br>");
+
+$result = mysqli_query($conn, $sql);
+     if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1" class="tabelka_moja">');
+echo ("<tr><th>liczba_pracownikow</th></tr>");
+while($row = mysqli_fetch_assoc($result)) {
+    echo ('<tr>');
+    echo ("<td>".$row['liczba_pracownikow']."</td>");
+    echo ('</tr>');
+}
+echo ('</table>');
 ?>
      </body>
 </html>
