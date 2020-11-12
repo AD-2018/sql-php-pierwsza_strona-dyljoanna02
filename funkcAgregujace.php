d0e08b52868924dedeb57f7a24b0a9e9386a53a2
Joanna Dyl <73647813+DylJoanna022@users.noreply.github.com>
2020-11-12 10:58:12 +0100

    Update funkcAgregujace.php

--- funkcAgregujace.php
+++ funkcAgregujace.php
@@ -53,6 +53,26 @@ while($row = mysqli_fetch_assoc($result)) {
     echo ('</tr>');
 }
 echo ('</table>');
+     
+$sql = "SELECT sum(zarobki) as suma FROM pracownicy WHERE imie NOT LIKE '%a' AND (dzial=2 OR dzial=3)";
+echo("<h3>ZADANIE 2</h3>");
+echo("<li>".$sql."<br><br>");
+
+$result = mysqli_query($conn, $sql);
+     if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1" class="tabelka_moja">');
+echo ("<tr><th>suma</th></tr>");
+while($row = mysqli_fetch_assoc($result)) {
+    echo ('<tr>');
+    echo ("<td>".$row['suma']."</td>");
+    echo ('</tr>');
+}
+echo ('</table>');
 ?>
      </body>
 </html>
