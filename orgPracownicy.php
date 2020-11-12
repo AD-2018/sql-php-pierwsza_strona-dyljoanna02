387791314769c6594cdc4ad802cb620d69bcc031
Joanna Dyl <73647813+DylJoanna022@users.noreply.github.com>
2020-11-12 10:47:03 +0100

    Update orgPracownicy.php

--- orgPracownicy.php
+++ orgPracownicy.php
@@ -194,6 +194,27 @@ while($row = mysqli_fetch_assoc($result)) {
     echo ('</tr>');
 }
 echo ('</table>');
+echo ("<h2>LIMIT</h2>");
+     
+$sql = "SELECT imie,zarobki,nazwa_dzial FROM pracownicy,organizacja WHERE id_org=dzial AND dzial=4 ORDER BY zarobki desc LIMIT 2";
+echo("<h3>ZADANIE 1</h3>");
+echo("<li>".$sql."<br><br>");
+
+$result = mysqli_query($conn, $sql);
+     if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1" class="tabelka_moja">');
+echo ("<tr><th>imie</th><th>zarobki</th><th>nazwa_dzial</th></tr>");
+while($row = mysqli_fetch_assoc($result)) {
+    echo ('<tr>');
+    echo ("<td>".$row['imie']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td>");
+    echo ('</tr>');
+}
+echo ('</table>');
 ?>
   </body>
 </html>
