98104275338fe590fac6b177bfd2527f322e8b5f
Joanna Dyl <73647813+DylJoanna022@users.noreply.github.com>
2020-11-12 11:03:37 +0100

    Update funkcAgregujace.php

--- funkcAgregujace.php
+++ funkcAgregujace.php
@@ -113,6 +113,26 @@ while($row = mysqli_fetch_assoc($result)) {
     echo ('</tr>');
 }
 echo ('</table>');
+     
+$sql = "SELECT avg(zarobki) as srednia FROM pracownicy WHERE imie NOT LIKE '%a' AND (dzial=1 OR dzial=2)";
+echo("<h3>ZADANIE 6</h3>");
+echo("<li>".$sql."<br><br>");
+
+$result = mysqli_query($conn, $sql);
+     if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1" class="tabelka_moja">');
+echo ("<tr><th>srednia</th></tr>");
+while($row = mysqli_fetch_assoc($result)) {
+    echo ('<tr>');
+    echo ("<td>".$row['srednia']."</td>");
+    echo ('</tr>');
+}
+echo ('</table>');
 ?>
      </body>
 </html>
