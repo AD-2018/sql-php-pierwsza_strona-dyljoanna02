9e4dad512f3f49760e6ad6415d843da2824ef6c0
Joanna Dyl <73647813+DylJoanna022@users.noreply.github.com>
2020-11-13 11:37:21 +0100

    Update dataczas.php

--- dataczas.php
+++ dataczas.php
@@ -371,6 +371,26 @@ while($row = mysqli_fetch_assoc($result)) {
     echo ('</tr>');
 }
 echo ('</table>');
+    
+$sql = "SELECT DATEDIFF(CURDATE(),"23-04-2002") as dni, DATEDIFF(CURDATE(),"23-04-2002")*24 as godziny, DATEDIFF(CURDATE(),"23-04-2002")*24*60 as minuty";
+echo("<h3>ZADANIE 6</h3>");
+echo("<li>".$sql."<br><br>");
+
+$result = mysqli_query($conn, $sql);
+     if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1" class="tabelka_moja">');
+echo ("<tr><th>dni</th><th>godziny</th><th>minuty</th></tr>");
+while($row = mysqli_fetch_assoc($result)) {
+    echo ('<tr>');
+    echo ("<td>".$row['czas']."</td>");
+    echo ('</tr>');
+}
+echo ('</table>');
 ?>
 
 </body>
