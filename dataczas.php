1a85e84283703f8ec5eb3e9d31161e8b1a6b5def
Joanna Dyl <73647813+DylJoanna022@users.noreply.github.com>
2020-11-13 10:56:11 +0100

    Update dataczas.php

--- dataczas.php
+++ dataczas.php
@@ -274,6 +274,26 @@ while($row = mysqli_fetch_assoc($result)) {
 }
 echo ('</table>');
     echo("<h2>FORMATOWANIE</h2>");
+    
+$sql = "SELECT *, DATE_FORMAT(data_urodzenia,'%W-%m-%Y') from pracownicy;";
+echo("<h3>ZADANIE 2</h3>");
+echo("<li>".$sql."<br><br>");
+
+$result = mysqli_query($conn, $sql);
+     if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1" class="tabelka_moja">');
+echo ("<tr><th>imie</th><th>zarobki</th><th>data urodzenia</th><th>dzial</th></tr>");
+while($row = mysqli_fetch_assoc($result)) {
+    echo ('<tr>');
+    echo ("<td>".$row['imie']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td><td>".$row['dzial']."</td>");
+    echo ('</tr>');
+}
+echo ('</table>');
 ?>
 
 </body>
