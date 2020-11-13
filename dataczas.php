c1878885bbdbf4fc15c81ab0a5bed67111632cd8
Joanna Dyl <73647813+DylJoanna022@users.noreply.github.com>
2020-11-13 10:38:38 +0100

    Update dataczas.php

--- dataczas.php
+++ dataczas.php
@@ -234,6 +234,26 @@ while($row = mysqli_fetch_assoc($result)) {
 }
 echo ('</table>');
     
+$sql = "SELECT imie, DAY(YEAR(curdate())-YEAR(data_urodzenia)) as dni FROM pracownicy";
+echo("<h3>ZADANIE 12</h3>");
+echo("<li>".$sql."<br><br>");
+
+$result = mysqli_query($conn, $sql);
+     if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1" class="tabelka_moja">');
+echo ("<tr><th>imie</th><th>dni</th></tr>");
+while($row = mysqli_fetch_assoc($result)) {
+    echo ('<tr>');
+    echo ("<td>".$row['imie']."</td><td>".$row['dni']."</td>");
+    echo ('</tr>');
+}
+echo ('</table>');
+    
 ?>
 
 </body>
