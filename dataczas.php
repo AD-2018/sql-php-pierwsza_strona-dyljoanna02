dfbdee00a44319ba91eb5967270a853d98844659
Joanna Dyl <73647813+DylJoanna022@users.noreply.github.com>
2020-11-13 10:26:46 +0100

    Update dataczas.php

--- dataczas.php
+++ dataczas.php
@@ -174,6 +174,26 @@ while($row = mysqli_fetch_assoc($result)) {
 }
 echo ('</table>');
     
+$sql = "SELECT nazwa_dzial,YEAR(curdate())-YEAR(data_urodzenia) as wiek FROM pracownicy,organizacja WHERE id_org=dzial GROUP BY dzial ORDER BY wiek desc LIMIT 1";
+echo("<h3>ZADANIE 9</h3>");
+echo("<li>".$sql."<br><br>");
+
+$result = mysqli_query($conn, $sql);
+     if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1" class="tabelka_moja">');
+echo ("<tr><th>wiek</th><th>nazwa_dzial</th></tr>");
+while($row = mysqli_fetch_assoc($result)) {
+    echo ('<tr>');
+    echo ("<td>".$row['wiek']."</td><td>".$row['nazwa_dzial']."</td>");
+    echo ('</tr>');
+}
+echo ('</table>');
+    
 ?>
 
 </body>
