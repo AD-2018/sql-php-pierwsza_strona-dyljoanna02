8d46d42779b92281b539f4aa55dcda71ccb3d77c
Joanna Dyl <73647813+DylJoanna022@users.noreply.github.com>
2020-11-13 12:06:59 +0100

    Update dataczas.php

--- dataczas.php
+++ dataczas.php
@@ -446,6 +446,26 @@ while($row = mysqli_fetch_assoc($result)) {
     echo ('</tr>');
 }
 echo ('</table>');
+    
+$sql = "SELECT count(DATE_FORMAT(data_urodzenia, "%W")) as liczba_pracownikow FROM pracownicy where DATE_FORMAT(data_urodzenia, "%W")="Poniedziałek";";
+echo("<h3>ZADANIE 9</h3>");
+echo("<li>".$sql."<br><br>");
+
+$result = mysqli_query($conn, $sql);
+     if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1" class="tabelka_moja">');
+echo ("<tr><th>liczba_pracownikow</th></tr>");
+while($row = mysqli_fetch_assoc($result)) {
+    echo ('<tr>');
+    echo ("<td>".$row['liczba_pracownikow']."</td></td>");
+    echo ('</tr>');
+}
+echo ('</table>');
 ?>
 
 </body>
