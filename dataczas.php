4a64f0937da056fb6637ad9c9d278338898f67b1
Joanna Dyl <73647813+DylJoanna022@users.noreply.github.com>
2020-11-13 09:34:48 +0100

    Create dataczas.php

---
+++ dataczas.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div>
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-dyljoanna02">Github</a><br/>
+    <a href="orgPracownicy.php">Pracownicy i Organizacja </a>
+    <a href="funkcAgregujace.php">Funkcje Agregujace</a><br/>
+    </div>
+<?php
+echo("Jestem tutaj: <br>Data i Czas</br>");
+    
+    require_once "connect.php";
+    
+?>
+
+</body>
+</html>
