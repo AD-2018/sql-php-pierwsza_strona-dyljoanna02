ba6e09f5ca149345ff95d72a4c4d81f970a46164
Joanna Dyl <73647813+DylJoanna022@users.noreply.github.com>
2020-11-13 10:15:18 +0100

    Update dataczas.php

--- dataczas.php
+++ dataczas.php
@@ -114,6 +114,26 @@ while($row = mysqli_fetch_assoc($result)) {
 }
 echo ('</table>');
     
+$sql = "SELECT sum(YEAR(curdate())-YEAR(data_urodzenia)) as wiek FROM pracownicy WHERE imie NOT LIKE '%a'";
+echo("<h3>ZADANIE 6</h3>");
+echo("<li>".$sql."<br><br>");
+
+$result = mysqli_query($conn, $sql);
+     if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1" class="tabelka_moja">');
+echo ("<tr><th>wiek</th></tr>");
+while($row = mysqli_fetch_assoc($result)) {
+    echo ('<tr>');
+    echo ("<td>".$row['wiek']."</td>");
+    echo ('</tr>');
+}
+echo ('</table>');
+    
 ?>
 
 </body>
