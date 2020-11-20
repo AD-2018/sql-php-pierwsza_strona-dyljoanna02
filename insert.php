f03d277a7f440afc03c74aedbf75c481bab4d5c5
Joanna Dyl <73647813+DylJoanna022@users.noreply.github.com>
2020-11-20 12:20:36 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -19,7 +19,7 @@ $sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodze
        VALUES (null, '".$_POST['imie']."', '".$_POST['dzial']."','".$_POST['zarobki']."','".$_POST['data_urodzenia']."')";
 
 if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
+  echo "<br>New record created successfully";
 } else {
 //informacja o ewentualnych błędach
   echo "Error: " . $sql . "<br>" . $conn->error;
