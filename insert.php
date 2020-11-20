beba0d897eaf3bc9143f8efe00e98be94a08f294
Joanna Dyl <73647813+DylJoanna022@users.noreply.github.com>
2020-11-20 12:19:21 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -15,9 +15,8 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO pracownicy (null, imie, dzial,zarobki,data_urodzenia)
-	      VALUES (null, '".$_POST['imie']."', '.$_POST['dzial'].', '.$_POST['zarobki'].','".$_POST['data_urodzenia'."']
-				";
+$sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
+       VALUES (null, '".$_POST['imie']."', '".$_POST['dzial']."','".$_POST['zarobki']."','".$_POST['data_urodzenia']."')";
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
