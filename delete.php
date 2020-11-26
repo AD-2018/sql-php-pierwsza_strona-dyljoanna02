1c2b9ff987e96440329fd7e22d4f561d3ef62243
Joanna Dyl <73647813+DylJoanna022@users.noreply.github.com>
2020-11-26 13:36:06 +0100

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,29 @@
+<?php
+echo("jestes w delete.php <br>");
+echo $_POST['id'];
+
+
+$servername = "mysql-joannadyl.alwaysdata.net";
+$username = "joannadyl";
+$password = "lollol92589@";
+$dbname = "joannadyl_sql";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+//definiujemy zapytanie $sql
+$sql = "DELETE FROM pracownicy WHERE id=".$_POST['id'];
+
+//wyświetlamy zapytanie $sql
+echo $sql;
+
+if ($conn->query($sql) === TRUE) {
+  echo "Record deleted successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
