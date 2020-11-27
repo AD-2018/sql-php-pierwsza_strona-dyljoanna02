0510b3a64af975967606e6520d931a00bb664af8
Joanna Dyl <73647813+DylJoanna022@users.noreply.github.com>
2020-11-27 11:45:03 +0100

    Update danedobazy.php

--- danedobazy.php
+++ danedobazy.php
@@ -38,7 +38,14 @@ echo('<table border="1" class="tabelka_moja">');
 echo ("<tr><th>id</th><th>imie</th><th>zarobki</th><th>data urodzenia</th><th>dzial</th></tr>");
 while($row = mysqli_fetch_assoc($result)) {
     echo ('<tr>');
-    echo ("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td><td>".$row['dzial']."</td>");
+   echo('<td>'.$row['id_pracownicy'].'</td>'.'<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td>'.'<td>'.$row['dzial'].'</td>'.'<td>'.$row['data_urodzenia'].'</td>'.
+
+         '<td>
+	<form action="delete.php" method="POST">
+        <input type="number" name="id" value="'.$row['id_pracownicy'].'"></br>
+        <input type="submit" value="Zapisz w zmiennej dolar_POST">
+    	</form>
+	</td>');
     echo ('</tr>');
 }
 echo ('</table>');
