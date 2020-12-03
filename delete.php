1abc4797224a07441fec3d4aa7c133d6002a89e4
Joanna Dyl <73647813+DylJoanna022@users.noreply.github.com>
2020-12-03 13:11:40 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -6,10 +6,6 @@ echo $_POST['id_pracownicy'];
 
 require_once "connect.php";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
 
 //definiujemy zapytanie $sql
 $sql = "DELETE FROM pracownicy WHERE id_pracownicy=".$_POST['id_pracownicy'];
