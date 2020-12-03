271be567b170587a20499c80a92a113798312f6a
Joanna Dyl <73647813+DylJoanna022@users.noreply.github.com>
2020-12-03 13:17:11 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -5,15 +5,7 @@ echo "<li>". $_POST['dzial'];
 echo "<li>". $_POST['zarobki'];
 echo "<li>". $_POST['data_urodzenia'];
 
-$servername = "mysql-joannadyl.alwaysdata.net";
-$username = "joannadyl";
-$password = "lollol92589@";
-$dbname = "joannadyl_sql";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+require_once "connect.php";
 
 $sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
        VALUES (null, '".$_POST['imie']."', '".$_POST['dzial']."','".$_POST['zarobki']."','".$_POST['data_urodzenia']."')";
