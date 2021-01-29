df3a075164dd6e75fc78461432d294c0dfafb13f
DylJoanna022 <joanna.dyll@gmail.com>
2021-01-29 12:15:51 +0100

    title

--- biblioteka/ksiazki.php
+++ biblioteka/ksiazki.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width">
 	<link rel="stylesheet" href="/style.css">
-  <title>Książki</title>
+  <title>Joanna Dyl</title>
 </head>
 <body>
 	<div class="container">
--- pracownicy/danedobazy.php
+++ pracownicy/danedobazy.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width">
 	<link rel="stylesheet" href="/style.css">
-  <title>Dane do Bazy</title>
+  <title>Joanna Dyl</title>
 </head>
 <body>
 	<div class="container">
