ed5b0a35e001c5f85869add77f6c6bb4ac15a473
DylJoanna022 <joanna.dyll@gmail.com>
2021-03-15 09:57:16 +0100

    sprawdzenie

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <link rel="stylesheet" href="style.css">
-<title>Joanna Dyl</title>
+<title>Joanna Dyl kl III ti</title>
 <link rel="apple-touch-icon" sizes="180x180" href="/icon/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="/icon/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="/icon/favicon-16x16.png">
