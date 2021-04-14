4a812e01c145cf1ddfbdb38cfa1640be9d29058a
DylJoanna022 <joanna.dyll@gmail.com>
2021-04-14 18:44:12 +0200

    grid

---
+++ grid/grid7/index7.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>CSS Grid</title>
+    <link rel="stylesheet" href="style7.css" />
+  </head>
+  <body>
+    <div class="container">
+    <div class="menu blue">
+    <a class="link" href="https://dyljoanna.herokuapp.com">Strona Glowna </a>
+    <a class="link" href="../grid1/index1.php">GRID1</a>
+        <a class="link" href="../grid2/index2.php">GRID2</a>
+        <a class="link" href="../grid3/index3.php">GRID3</a>
+        <a class="link" href="../grid4/index4.php">GRID4</a>
+        <a class="link" href="../grid5/index5.php">GRID5</a>
+        <a class="link" href="../grid6/index6.php">GRID6</a>
+        <a class="link" href="../grid7/index7.php">GRID7</a>
+        <a class="link" href="../grid8/index8.php">GRID8</a>
+        <a class="link" href="../grid9/index9.php">GRID9</a>
+        <a class="link" href="../grid10/index10.php">GRID10</a>
+        <a class="link" href="../grid11/index11.php">GRID11</a>
+        <a class="link" href="../grid12/index12.php">GRID12</a>
+    </div>
+      <header>
+        1
+      </header>
+      <nav>
+        2
+      </nav>
+      <main>
+        3
+      </main>
+      <aside>
+        5
+        </aside>
+      <footer>
+        4
+      </footer>
+      <div class="dod">
+          6
+      </div>
+      <div class="dod2">
+        7
+    </div>
+    </div>
+  </body>
+</html>
