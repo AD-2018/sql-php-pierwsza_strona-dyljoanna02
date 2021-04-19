93f451b67d603ebea2b2611802147c35895de451
DylJoanna022 <joanna.dyll@gmail.com>
2021-04-19 09:27:26 +0200

    li

--- menu.php
+++ menu.php
@@ -1,9 +1,11 @@
-<a class="link" href="https://dyljoanna.herokuapp.com">Strona Glowna </a>
-<a class="link" href="/pracownicy/orgPracownicy.php">Pracownicy i Organizacja </a>
-<a class="link" href="/pracownicy/dataczas.php">Data i Czas </a>
-<a class="link" href="/pracownicy/funkcAgregujace.php">Funkcje Agregujace</a>
-<a class="link" href="/pracownicy/formularz.html">Formularz</a>
-<a class="link" href="/pracownicy/danedobazy.php">Do Bazy</a>
-<a class="link" href="/biblioteka/ksiazki.php">Ksiazki</a><br/>
-<a class="link" href="/flexbox/index.html">Flexbox</a>
-<a class="link" href="/grid/index.html">GRID</a>
+<ul>
+<li><a class="link" href="https://dyljoanna.herokuapp.com">Strona Glowna </a></li>
+<li><a class="link" href="/pracownicy/orgPracownicy.php">Pracownicy i Organizacja </a></li>
+<li><a class="link" href="/pracownicy/dataczas.php">Data i Czas </a></li>
+<li><a class="link" href="/pracownicy/funkcAgregujace.php">Funkcje Agregujace</a></li>
+<li><a class="link" href="/pracownicy/formularz.html">Formularz</a></li>
+<li><a class="link" href="/pracownicy/danedobazy.php">Do Bazy</a></li>
+<li><a class="link" href="/biblioteka/ksiazki.php">Ksiazki</a></li>
+<li><a class="link" href="/flexbox/index.html">Flexbox</a></li>
+<li><a class="link" href="/grid/index.html">GRID</a></li>
+</ul>
