9e6a04d8987c5fa2103ae9ffd607d8e3ec4b698b
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2021-04-22 09:43:06 +0200

    Update delpp.php

--- grid/grid1/delpp.php
+++ grid/grid1/delpp.php
@@ -10,7 +10,7 @@ $sql = "DELETE FROM pracownik_projekt WHERE id_proj=".$_POST['id'];
 echo $sql;
 
 if ($conn->query($sql) === TRUE) {
-  echo "Record deleted successfully";
+
   header('Location: https://dyljoanna.herokuapp.com/grid/grid1/index1.php');
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
